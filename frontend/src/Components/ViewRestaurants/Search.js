a14819f71b533d020a45271ae485c741b54b5c07
liz.ontiveros <liz.ontiveros10.7@gmail.com>
2022-11-16 02:26:15 -0600

    Search layout

--- frontend/src/Components/ViewRestaurants/Search.js
+++ frontend/src/Components/ViewRestaurants/Search.js
@@ -1,11 +1,15 @@
+import { checkPropTypes } from 'prop-types'
 import React from 'react'
+import './Search.css';
 
-export default function Search() {
+export default function Search(props) {
 
     const [location, setLocation] = React.useState({city: "", zipcode: ""})
 
     function submitLocationToApi(){
         /** Code to fetch from Api */
+        props.setNeedSearch(prevNeedSearch => prevNeedSearch = !prevNeedSearch)
+        props.setNeedView(prevNeedView => prevNeedView = !prevNeedView)
     }
     
     function handleClick(event){
@@ -25,24 +29,33 @@ export default function Search() {
     }
 
     return (
-        <div>
-            <h1>Welcome!</h1>
-            <h3>To get started browsing restaurants please enter a city or zipcode.</h3>
-            <input 
-                type="text" 
-                placeholder="Zipcode" 
-                onChange={handleChange} 
-                name="zipcode"
-                value={location.zipcode}
-            />
-            <input 
-                type="text" 
-                placeholder="City" 
-                onChange={handleChange} 
-                name="city"
-                value={location.city}
-            />
-            <button type="submit" onClick={handleClick}>Submit</button>
+        <div className="search">
+            <section className='welcome'>
+                <h1>Welcome!</h1>
+                <img src={require('../../images/Whatever-4.png')} alt='main-logo'/>    
+            </section>
+            <section className='main'>
+                <h3>To get started browsing restaurants please enter a city or zipcode.</h3>
+                <section className='input'>
+                    <input className='zipcode'
+                        type="text" 
+                        placeholder="Zipcode" 
+                        onChange={handleChange} 
+                        name="zipcode"
+                        value={location.zipcode}
+                    />
+                    <input className='city'
+                        type="text" 
+                        placeholder="City" 
+                        onChange={handleChange} 
+                        name="city"
+                        value={location.city}
+                    />
+                </section>
+                <section className='submit'>
+                    <button type="submit" onClick={handleClick}>Submit</button>
+                </section>
+            </section>   
         </div>
     )
 }
